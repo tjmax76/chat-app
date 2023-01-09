41c869ab8d21561e7a1d6784bdc39063816e876b
tjmax76 <113315287+tjmax76@users.noreply.github.com>
2023-01-09 20:36:48 +0900

    画像のみでも送信可能となる画像のみでも送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,10 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
-  validates :content, presence: true
+  validates :content, presence: true, unless: :was_attached?
+  has_one_attached :image
+
+  def was_attached?
+    self.image.attached?
+  end
 end
